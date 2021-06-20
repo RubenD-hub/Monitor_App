5309d86d8ff839a73322b253e86e449d7b2fe9ec
Rubén Sánchez <rubenlop98@gmail.com>
2021-06-20 16:10:19 -0500

    Switch button ready

--- pages/devices.vue
+++ pages/devices.vue
@@ -75,7 +75,14 @@
             label="Template"
           ></el-table-column>
           <el-table-column label="Actions">
+
             <div slot-scope="{row, $index}">
+                
+                {{row.saverRule}}
+                <el-tooltip content="Database Saver">
+                    <base-switch color="black" :value= "row.saverRule" on-text="On" off-text="Off" @click="updateSaverRuleStatus($index)"></base-switch>
+                </el-tooltip>
+                
                 <el-tooltip content="Delete" :open-delay="300" placement="top">
                     <base-button type="danger" size="sm" icon @click="deleteDevice(row)">
                         <i class="tim-icons icon-simple-remove"></i>
@@ -108,19 +115,22 @@ export default {
           name: "Temp",
           dId: "8888",
           templateName: "Temp Sensor",
-          templateId: "24244242424"
+          templateId: "24244242424",
+          saverRule: false
         },
         {
           name: "SaO2",
           dId: "2222",
           templateName: "SaO2 Sensor",
-          templateId: "2432415435"
+          templateId: "2432415435",
+          saverRule: true
         },
         {
           name: "FR",
           dId: "7777",
           templateName: "FR Sensor",
-          templateId: "5345635364"
+          templateId: "5345635364",
+          saverRule: false
         }
       ]
     };
@@ -129,6 +139,9 @@ export default {
       deleteDevice(device){
           alert("Deleting " + device.name)
 
+      },
+      updateSaverRuleStatus(index){
+          this.devices[index].saverRule = !this.devices[index].saverRule;
       }
   }
 };
