12dfd1f472beb6444626cbedc789a2e01dbf18c9
Rubén Sánchez <rubenlop98@gmail.com>
2021-07-01 15:15:14 -0500

    list resources emqx

--- api/index.js
+++ api/index.js
@@ -25,6 +25,8 @@ app.use("/api", require("./routes/devices.js"));
 app.use("/api", require("./routes/users.js"));
 app.use("/api", require("./routes/templates.js"));
 app.use("/api", require("./routes/webhooks.js"));
+app.use("/api", require("./routes/emqxapi.js"));
+
 module.exports = app;
 
 // Listener
